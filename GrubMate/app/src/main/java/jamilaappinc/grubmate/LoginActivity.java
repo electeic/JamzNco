83a74938c72eb35de653a5afe8342a63a7c1fd42
Zhehao Zhang <zhehaozh@usc.edu>
2017-10-07 12:21:09 -0700

    facebook sync + fixed login page sync

--- GrubMate/app/src/main/java/jamilaappinc/grubmate/LoginActivity.java
+++ GrubMate/app/src/main/java/jamilaappinc/grubmate/LoginActivity.java
@@ -14,7 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 
         //get intent data
         Intent i = getIntent();
