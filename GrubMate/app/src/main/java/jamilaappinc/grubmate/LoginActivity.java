8d343377a05222f75a44bbfe79f195f8f5f07470
ivan chen <ivanc@usc.edu>
2017-10-10 02:29:20 -0700

    facebook api works, login screen won't brick hopefully.

--- GrubMate/app/src/main/java/jamilaappinc/grubmate/LoginActivity.java
+++ GrubMate/app/src/main/java/jamilaappinc/grubmate/LoginActivity.java
@@ -6,15 +6,29 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
 
+
     public static final String EXTRA_POSITION = "extra_position";
+    private static final String TAG = "TAG";
+    CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.setApplicationId("140229246720277");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.fragment_login);
 
         //get intent data
         Intent i = getIntent();
@@ -22,16 +36,37 @@ public class LoginActivity extends AppCompatActivity {
         //TODO modify for id
         int pos = i.getIntExtra(EXTRA_POSITION, -1);
 
-        //Create fragment
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment f = fm.findFragmentById(R.id.fragment_container);
+        FacebookSdk.sdkInitialize(this);
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile", "user_friends");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+//                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
 
-        if (f == null ) {
-            //TODO modify for id
-            f = LoginFragment.newInstance(pos);
-        }
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, f);
-        fragmentTransaction.commit();
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
--- GrubMate/app/src/main/java/jamilaappinc/grubmate/LoginFragment.java
+++ GrubMate/app/src/main/java/jamilaappinc/grubmate/LoginFragment.java
@@ -4,9 +4,17 @@ package jamilaappinc.grubmate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -18,6 +26,9 @@ import java.util.TimerTask;
 public class LoginFragment extends Fragment {
 
     public static final String ARGS_POSITION = "args_position";
+    private static final String TAG = "TAG";
+    CallbackManager mCallbackManager;
+
 
     public LoginFragment() {
         // Required empty public constructor
@@ -45,6 +56,11 @@ public class LoginFragment extends Fragment {
         }
     }
 
+    public CallbackManager getCallbackManager()
+    {
+        return mCallbackManager;
+    }
+
     /////////////////////////////////////////////////////////////////////
 
     //TODO modify for id
@@ -61,8 +77,35 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        startSplashTimer();
-        return inflater.inflate(R.layout.fragment_login, container, false);
+//        startSplashTimer();
+        FacebookSdk.setApplicationId("140229246720277");
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        FacebookSdk.sdkInitialize(getActivity());
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) v.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile", "user_friends");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+//                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+        });
+
+        return v;
     }
+
 
 }
