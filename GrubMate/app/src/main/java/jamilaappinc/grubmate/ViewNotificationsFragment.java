67172047b3972449492934c8773a93c6765034bc
DESKTOP-CIAEAFE\melody <chaimelo@usc.edu>
2017-10-13 16:14:31 -0700

    added menu button to subscriptions, notifications, groups

--- GrubMate/app/src/main/java/jamilaappinc/grubmate/ViewNotificationsFragment.java
+++ GrubMate/app/src/main/java/jamilaappinc/grubmate/ViewNotificationsFragment.java
@@ -1,6 +1,7 @@
 package jamilaappinc.grubmate;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -27,6 +28,7 @@ public class ViewNotificationsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    android.support.design.widget.FloatingActionButton floatButton;
 
     private OnFragmentInteractionListener mListener;
 
@@ -63,7 +65,19 @@ public class ViewNotificationsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_notifications, container, false);
+        View v = inflater.inflate(R.layout.fragment_view_notifications, container, false);
+
+        floatButton = (android.support.design.widget.FloatingActionButton) v.findViewById(R.id.menu_from_main);
+
+        floatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MenuActivity.class);
+                startActivityForResult(intent, 0);
+                getActivity().finish();
+            }
+        });
+        return v;
     }
 
 /*    // TODO: Rename method, update argument and hook method into UI event
--- GrubMate/app/src/main/java/jamilaappinc/grubmate/ViewSubscriptionsFragment.java
+++ GrubMate/app/src/main/java/jamilaappinc/grubmate/ViewSubscriptionsFragment.java
@@ -1,6 +1,7 @@
 package jamilaappinc.grubmate;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -25,6 +26,7 @@ public class ViewSubscriptionsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    android.support.design.widget.FloatingActionButton floatButton;
 
 //    private OnFragmentInteractionListener mListener;
 
@@ -62,7 +64,19 @@ public class ViewSubscriptionsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_subscriptions, container, false);
+        View v = inflater.inflate(R.layout.fragment_view_notifications, container, false);
+
+        floatButton = (android.support.design.widget.FloatingActionButton) v.findViewById(R.id.menu_from_main);
+
+        floatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MenuActivity.class);
+                startActivityForResult(intent, 0);
+                getActivity().finish();
+            }
+        });
+        return v;
     }
 /*
     // TODO: Rename method, update argument and hook method into UI event
