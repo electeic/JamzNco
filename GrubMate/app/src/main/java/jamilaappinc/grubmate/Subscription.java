9c4d8820c4ad1de8cdbbf818869c431a2ce919ed
Zhehao Zhang <zhehaozh@usc.edu>
2017-10-18 03:22:05 -0700

    fixed sub

--- GrubMate/app/src/main/java/jamilaappinc/grubmate/Subscription.java
+++ GrubMate/app/src/main/java/jamilaappinc/grubmate/Subscription.java
@@ -30,7 +30,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
     }
 
     public Subscription( String title,String description, Date mStartDate, Date mEndDate,  ArrayList<String> mCategories,
-                         ArrayList<String> mTags,String mUser, boolean mHomeMade, String mId, ArrayList<Post> mPosts) {
+                         ArrayList<String> mTags,String mUser, boolean mHomeMade) {
         this.mDescription = description;
         this.mTitle = title;
         this.mUserAuthorId = mUser;
@@ -40,8 +40,8 @@ import java.util.concurrent.CopyOnWriteArrayList;
         this.mCategories = mCategories;
         this.mHomeMade = mHomeMade;
         mActive = Boolean.TRUE;
-        this.mId = mId;
-        this.mPosts = mPosts;
+        //this.mId = mId;
+        //this.mPosts = mPosts;
     }
 
     public boolean getmActive(){return mActive;}
