c1ba886b03bf2b4d999115bdb870bc283c4f6509
Erica Jung <ericajung@Ericas-MacBook-Pro.local>
2017-10-31 00:51:48 -0700

    fixed sub test

--- GrubMate/app/src/test/java/jamilaappinc/grubmate/SubscriptionFragmentTest.java
+++ GrubMate/app/src/test/java/jamilaappinc/grubmate/SubscriptionFragmentTest.java
@@ -22,11 +22,6 @@ public class SubscriptionFragmentTest {
         assertEquals(subCreated,false);
     }
     @Test
-    public void createSubNoServings() throws Exception {
-        boolean subCreated = createsSubscriptionFragment.checkAllFilledAndWriteDBTest("Honey Cake","Milk", "SAL", "", "honey,cake", "chinese,american", new Date(2017,10,30,10,8), new Date(2017,10,31,10,8));
-        assertEquals(subCreated,false);
-    }
-    @Test
     public void createSubNoTags() throws Exception {
         boolean subCreated = createsSubscriptionFragment.checkAllFilledAndWriteDBTest("Honey Cake","Milk", "SAL", "3", "", "chinese,american", new Date(2017,10,30,10,8), new Date(2017,10,31,10,8));
         assertEquals(subCreated,false);
